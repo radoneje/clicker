e07152ce0689116ba87068d3a8ebdbfcec418226
radoneje <den.shevchenko@gmail.com>
2021-11-19 22:29:48 +0300

    d

--- soc.js
+++ soc.js
@@ -12,13 +12,20 @@ let soc=function (server){
             workers=workers.filter(c=>c.id!=socket.id);
         });
         socket.on("helloClient", (arg) => {
-            clients.push({id:socket.id, workerId:JSON.parse(arg).workerId})
+            clients.push({id:socket.id, workerId:JSON.parse(arg).workerId, socket})
         });
         socket.on("helloWorker", (arg) => {
             console.log("helloWorker");
-            var w={id:socket.id, workerId:workers.length+1}
+            var w={id:socket.id, workerId:workers.length+1, socket}
             workers.push(w)
             socket.emit("readyWorker", w.workerId);
+        });
+        socket.on("message", (arg) => {
+            var r=JSON.parse(arg);
+            workers.forEach(w=>{
+                if(w.workerId==arg.workerId)
+                    w.socket.emit(arg.cmd);
+            })
         });
     })
 }
